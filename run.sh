dc60a9c5f74463c20a38343fc622271bf86f008a
Andrew Teich <andrewteich@me.com>
2014-11-26 19:45:44 -0800

    works with agruments flag

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ then
   #echo 'Current IP: ' $currentIP
   #echo $2
   #mesos-slave --master=zk://10.9.0.1:2181/var/zookeeper/ --ip=$currentIP --port=5051 --attributes=$2 > mesos.log
-  nohup sudo mesos-slave --master=zk://10.9.0.1:2181/var/zookeeper/ --ip=$currentIP --port=505 0<&- &>mesos.log &
+  nohup sudo mesos-slave --master=zk://10.9.0.1:2181/var/zookeeper/ --ip=$currentIP --port=505 --attributes=$2 0<&- &>mesos.log &
 else
   echo 'KILL MESOS!'
   $1
