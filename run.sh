e118982121115344fe761448af1fb8e5e61becb7
Andrew Teich <andrewteich@me.com>
2014-11-29 16:42:29 -0800

    Fixed attributes

--- run.sh
+++ run.sh
@@ -6,10 +6,11 @@ then
   sudo service openvpn start client
   currentIP=$(ifconfig tun0 | grep "inet addr" | awk -F: '{print $2'} | awk '{print $1}')
   #echo $currentIP > mesosIP.log
-  #echo 'Current IP: ' $currentIP
-  #echo $2
+  echo 'Current IP:'$currentIP
+  echo 'Args: ' $2
   #mesos-slave --master=zk://10.9.0.1:2181/var/zookeeper/ --ip=$currentIP --port=5051 --attributes=$2 > mesos.log
-  nohup sudo mesos-slave --master=zk://10.9.0.1:2181/var/zookeeper/ --ip=$currentIP --port=505 --attributes=$2 0<&- &>mesos.log &
+  #nohup sudo mesos-slave --master=zk://10.9.0.1:2181/var/zookeeper/ --ip=$currentIP --port=5051 --attributes=$2 0 &
+  sudo mesos-slave --master=zk://10.9.0.1:2181/var/zookeeper/ --ip=$currentIP --port=5051 --attributes=$2 0<&- &>mesos.log &
 else
   echo 'KILL MESOS!'
   $1
